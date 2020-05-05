fa61387494677c4ee84cbabafb0108898b32d558
Tair Shauli <tairsha@users.noreply.github.com>
2020-05-05 18:13:32 +0300

    First batch of analyses

---
+++ calculate_ha.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pandas as pd
+
+import project_utilities as utils
+
+
+def matrix_power(hc1, power):
+    return pd.DataFrame(np.linalg.matrix_power(hc1, power), index = hc1.index, columns = hc1.columns)
+
+
+def aa_info(aa, codon_count_series, aa_sum_ref):
+    """
+    given an aa a, return a list of tuples: first in the tuple is a codon that codes for a,
+    and second is the codon frequency wrt all other codons that code for a
+    :param codon_count_series: a pandas Series containing the number of appearances of each codon in the refrenence genome
+    :param aa: a string representing an amino-acid
+    :return: dict of codon string and it's amino-acid relative frequency
+    """
+    # calculate each codon's frequency, within its multiplicity
+    return {c: codon_count_series[c]/aa_sum_ref[aa] for c in utils.aa_to_codon_dct[aa]}
+
+
+def build_aa_matrix(codon_matrix, codon_count_series):
+    """
+    Construct amino-acid substitution matrix from codon substitution matrix
+    :param codon_matrix: codon substitution matrix pandas DataFrame
+    :param codon_count_series: a pandas Series containing the number of appearances of each codon in the refrenence genome
+    :return:
+    """
+    # init empty matrices
+    aa_matrix = pd.DataFrame(0, index = utils.aa_lst, columns = utils.aa_lst, dtype = float)
+    temp_matrix = pd.DataFrame(0, index = utils.aa_lst, columns = utils.codon_lst_non_stop)
+
+    # deduce the amino-acid's codon usage distribution
+    aa_sum_ref = {aa: 0 for aa in utils.aa_lst}
+    for aa in utils.aa_lst:
+        for codon in utils.aa_to_codon_dct[aa]:
+            aa_sum_ref[aa] += codon_count_series[codon] # calculate codon usage distribution from the codon counts for normalization
+
+    aa_codon_distribution_ref_dct = {aa: aa_info(aa, codon_count_series, aa_sum_ref) for aa in utils.aa_lst}
+
+    # update rows of substitution matrix
+    for aa in utils.aa_lst:
+        for codon in utils.aa_to_codon_dct[aa]:
+            temp_matrix.loc[aa, :] += codon_matrix.loc[codon, :] * aa_codon_distribution_ref_dct[aa][codon]
+
+    # update columns of substitution matrix
+    for aa in utils.aa_lst:
+        for codon in utils.aa_to_codon_dct[aa]:
+            aa_matrix.loc[:, aa] += temp_matrix.loc[:, codon]
+
+    return aa_matrix
+
+
+def calc_ha(codon_substitution_matrix, power, codon_count_series):
+    """
+    Construct amino-acid substitution matrix from codon substitution matrix which is taken to the specified power
+    :param codon_substitution_matrix: pandas DataFrame, matrix should be of the first power
+    :param power: power that HC1 is to be taken to before converting to amino-acid resolution
+    :param codon_count_series: a pandas Series containing the number of appearances of each codon in the reference genome
+    :return: amino-acid substitution matrix to the specified power in a pandas DataFrame
+    """
+    codon_substitution_matrix = matrix_power(codon_substitution_matrix, power)
+    return build_aa_matrix(codon_substitution_matrix, codon_count_series)
---
+++ deduce_substiution_matrices.py
@@ -0,0 +1,119 @@
+import pandas as pd
+
+import project_utilities as utils
+
+# string headers of variant DataFrame
+REF_CODON = 'REF_CODON'
+ALT_CODON = 'ALT_CODON'
+
+REF_NUC = 'REF_NUCLEOTIDE'
+ALT_NUC = 'ALT_NUCLEOTIDE'
+
+AF = 'ALLELE_FREQUENCY'
+
+
+def count_af(substitution_matrix, variant_df, subject_lst, subject_headers):
+    """
+    Calculate substitution matrix, not normalized.
+    :param substitution_matrix: empty DataFrame which will contain summed substitution frequencies
+    :param variant_df: DataFrame which contains aggregated variant information: for each genomic position,
+    it should contain at least the source codon, target codon, source nucleotide, target nucleotide, and the variant's allele frequency
+    :param subject_lst: list of codons\nucleotides
+    :param subject_header: tuple of header of reference and alternative columns
+    :return: not normalized substitution matrix
+    """
+    ref_str, alt_str = subject_headers  # get header string for DataFrame column access
+    for ref in subject_lst:
+        ref_df = variant_df[variant_df[ref_str] == ref]  # use only variant in which the reference is ref
+        for alt in subject_lst:
+            if alt == ref:  # skip synonymous as data does not contain synonymous substitutions
+                continue
+            ref_alt_df = ref_df[ref_df[alt_str] == alt]  # use only variant in which the alternative is alt
+            substitution_matrix.at[ref, alt] = ref_alt_df[AF].sum()  # sum corresponding allele frequencies into substitution matrix
+
+    return substitution_matrix
+
+
+def get_nuc_counts(codon_counts):
+    """
+    Count the number of appearances of each nucleotide in the reference genome
+    :param codon_counts: A pandas Series containing the number of appearances of each codon in the reference genome
+    :return:  pandas Series containing the number of appearances of each nucleotide in the reference genome
+    """
+    nuc_counts = pd.Series(0, index = utils.nucleotides_lst)
+    for codon in codon_counts.index:
+        for nuc in codon:
+            nuc_counts[nuc] += codon_counts.loc[codon]
+    return nuc_counts
+
+
+def normalise_matrix(substitution_matrix, counts):
+    """
+    Row-normalisation of the substitution matrix
+    :param substitution_matrix: DataFrame which contains summed substitution frequencies
+    :param counts: a pandas Series containing the number of appearances of each codon, for normalization
+    :return: row-normalised DataFrame
+    """
+    for i in substitution_matrix.index:
+        curr_counts = int(counts[i])
+        if curr_counts != 0:
+            substitution_matrix.loc[i, :] /= curr_counts
+            substitution_matrix.at[i, i] = 1 - substitution_matrix.loc[i, :].sum()
+        else:
+            substitution_matrix.loc[i, :] = 0
+
+    return substitution_matrix
+
+
+def calculate_substitution_matrix(variant_df, subject_lst, subject_header, normalize = True, counts = None):
+    """
+    calculate a nucleotide/codon substitution matrix according to aggregated variant information
+    :param variant_df: DataFrame which contains aggregated variant information: for each genomic position,
+    it should contain at least the source codon, target codon, source nucleotide, target nucleotide, and the variant's allele frequency
+    :param subject_lst: list of codons\nucleotides
+    :param subject_header: tuple of header of reference and alternative columns
+    :param counts: a pandas Series containing the number of appearances of each codon/nucleotide, for normalization. Defult value is None.
+    Will be used only if normalize is set to True.
+    :param normalize: boolean indicating whether the substitution matrix is to be normalized.
+    :return: substitution matrix
+    """
+
+    substitution_matrix = utils.create_zero_matrix(subject_lst)
+    substitution_matrix = count_af(substitution_matrix, variant_df, subject_lst, subject_header)
+
+    if normalize:
+        substitution_matrix = normalise_matrix(substitution_matrix, counts)
+
+    return substitution_matrix
+
+
+def calculate_hc1(variant_df, normalize = True, codon_counts = None):
+    """
+    calculate a codon substitution matrix according to aggregated variant information
+    :param variant_df: DataFrame which contains aggregated variant information: for each genomic position,
+    it should contain at least the source codon, target codon, source nucleotide, target nucleotide, and the variant's allele frequency
+    :param codon_counts: a pandas Series containing the number of appearances of each codon, for normalization. Defult value is None.
+    Will be used only if normalize is set to True.
+    :param normalize: boolean indicating whether the substitution matrix is to be normalized.
+    :return: codon substitution matrix
+    """
+
+    return calculate_substitution_matrix(variant_df, utils.codon_lst_non_stop, (REF_CODON, ALT_CODON),
+                                         normalize = normalize, counts = codon_counts)
+
+
+def calculate_hn1(variant_df, normalize = True, codon_counts = None):
+    """
+    calculate a nucleotide substitution matrix according to aggregated variant information
+    :param variant_df: DataFrame which contains aggregated variant information: for each genomic position,
+    it should contain at least the source codon, target codon, source nucleotide, target nucleotide, and the variant's allele frequency
+    :param normalize: boolean indicating whether the substitution matrix is to be normalized.
+    :param nuc_counts: a pandas Series containing the number of appearances of each nucleotide, for normalization. Defult value is None.
+    Will be used only if normalize is set to True.
+    :return: nucleotide substitution matrix
+    """
+
+    nuc_counts = get_nuc_counts(codon_counts)
+
+    return calculate_substitution_matrix(variant_df, utils.nucleotides_lst, (REF_NUC, ALT_NUC),
+                                         normalize = normalize, counts = nuc_counts)
